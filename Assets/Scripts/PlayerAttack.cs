d4e5c2c3377e911cb300b8de31b9a184089f8ada
Mithat <mithatsirmali@gmail.com>
2025-10-17 14:25:37 +0300

    Animation of AttackPoint

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,67 +1,46 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic; 
 
 public class PlayerAttack : MonoBehaviour
 {
-    [Header("Attack Settings")]
     public float damage = 20f;
-    public float attackDuration = 0.2f;
 
-    [Header("References")]
-    [SerializeField] private Collider2D attackCollider;
-    [SerializeField] private Transform attackPoint;
     private Animator anim;
-    private SpriteRenderer spr;
-
-    private List<Collider2D> hitEnemies = new List<Collider2D>(); 
+    [SerializeField] private Collider2D attackCollider;
 
     void Start()
     {
         anim = GetComponent<Animator>();
-        spr = GetComponent<SpriteRenderer>();
+
         if (attackCollider != null)
             attackCollider.enabled = false;
-    }
-
-    void Update()
-    {
-        if (attackCollider != null && attackPoint != null)
-        {
-            attackCollider.transform.position = attackPoint.position;
-
-            Vector3 scale = attackCollider.transform.localScale;
-            scale.x = spr.flipX ? -Mathf.Abs(scale.x) : Mathf.Abs(scale.x);
-            attackCollider.transform.localScale = scale;
-        }
     }
 
     public void Attack()
     {
         anim.SetTrigger("Attack");
-        hitEnemies.Clear(); 
-        StartCoroutine(EnableColliderMomentarily());
     }
 
-    private IEnumerator EnableColliderMomentarily()
+    public void EnableAttackCollider()
     {
-        attackCollider.enabled = true;
-        yield return new WaitForSeconds(attackDuration);
-        attackCollider.enabled = false;
+        if (attackCollider != null)
+            attackCollider.enabled = true;
+    }
+
+    public void DisableAttackCollider()
+    {
+        if (attackCollider != null)
+            attackCollider.enabled = false;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Enemy") && !hitEnemies.Contains(other))
+        if (other.CompareTag("Enemy"))
         {
             Enemy enemy = other.GetComponent<Enemy>();
-            
+
             if (enemy != null)
             {
-                // Düşmana hasar ver
                 enemy.GetDamage(damage);
-                
-                hitEnemies.Add(other); 
             }
         }
     }
